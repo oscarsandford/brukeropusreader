309e0609027270e6906e04a54f098d743c679f4b
Oscar Sandford <47612002+oscarsandford@users.noreply.github.com>
2023-01-31 19:16:19 -0800

    Remove scipy import and interpolate function
    Interpolation was only used in the example, and needed nowhere else. Slimmer dependencies this way. Waiting on PR#24 into master :)

--- brukeropusreader/opus_data.py
+++ brukeropusreader/opus_data.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.interpolate import interp1d
 
 
 class OpusData(dict):
@@ -14,10 +13,3 @@ class OpusData(dict):
             return x_no_unit
         else:
             return 10_000_000 / x_no_unit
-
-    def interpolate(self, start, stop, num, spec_name="AB"):
-        xav = self.get_range(spec_name=spec_name)
-        yav = self[spec_name]
-        iwave_nums = np.linspace(start, stop, num)
-        f2 = interp1d(xav, yav, kind="cubic", fill_value="extrapolate")
-        return iwave_nums, f2(iwave_nums)
